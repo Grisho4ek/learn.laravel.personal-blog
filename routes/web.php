e93e22edb349b016186e8d29847988af26aea932
Grisho4ek <shgrt@ukr.net>
2019-05-18 22:16:56 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+// урок 42
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PublicController@index')->name('index');
+
+Route::get('post/{id}', 'PublicController@singlePost')->name('singlePost');
+
+Route::get('about', 'PublicController@about')->name('about');
+
+Route::get('contact', 'PublicController@contact')->name('contact');
+
+Route::post('contact', 'PublicController@contactPost')->name('contactPost');
+
+Auth::routes();
+
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard','AdminController@dashboard')->name('adminDashboard');
+});
